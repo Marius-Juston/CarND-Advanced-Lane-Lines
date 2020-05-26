f3f4e7f9564961a72dd60ac59f537f201e8a8ac4
Marius-Juston <Marius.juston@hotmail.fr>
2020-05-26 12:47:55 -0400

    started undistortion code

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -34,7 +34,64 @@ def color_threshold(image, params):
     return hsl_mask
 
 
+def calibrate_camera(params):
+    grid_size = params['chessboard_size']
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((grid_size[0] * grid_size[1], 3), np.float32)
+    objp[:, :2] = np.mgrid[0:grid_size[0], 0:grid_size[1]].T.reshape(-1, 2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = []  # 3d points in real world space
+    imgpoints = []  # 2d points in image plane.
+
+    # Make a list of calibration images
+    images = glob.glob(f'{params["image_calib_folder"]}/calibration*.jpg')
+
+    # Step through the list and search for chessboard corners
+    for idx, fname in enumerate(images):
+        img = cv2.imread(fname)
+        img_size = (img.shape[1], img.shape[0])
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Find the chessboard corners
+        ret, corners = cv2.findChessboardCorners(gray, grid_size, None)
+
+        # If found, add object points, image points
+        if ret:
+            objpoints.append(objp)
+            imgpoints.append(corners)
+
+            # Draw and display the corners
+            cv2.drawChessboardCorners(img, grid_size, corners, ret)
+            cv2.imshow('img', img)
+            cv2.waitKey(500)
+
+    cv2.destroyAllWindows()
+
+    # Do camera calibration given object points and image points
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
+
+    # Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
+    dist_pickle = {"mtx": mtx, "dist": dist}
+    pickle.dump(dist_pickle, open(params['undistort'], "wb"))
+
+    return mtx, dist
+
+
+def undistort(image, params):
+    if os.path.exists(params['undistort']):
+        coefficients = pickle.load(open(params['undistort'], 'rb'))
+        mtx = coefficients['mtx']
+        dist = coefficients['dist']
+    else:
+        mtx, dist = calibrate_camera(params)
+
+    return cv2.undistort(image, mtx, dist, None, mtx)
+
+
 def image_pipeline(image, params):
+    undistorted = undistort(image, params)
 
     gradients = gradient_threshold(image, params)
     colors = color_threshold(image, params)
