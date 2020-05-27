2504ac6a0be3f23e561265e78709fa58b7430616
Marius-Juston <Marius.juston@hotmail.fr>
2020-05-26 22:13:25 -0400

    Added coefficient running means

--- scripts/lane.py
+++ scripts/lane.py
@@ -1,7 +1,8 @@
+import numpy as np
+
+
 class Line:
-    def __init__(self):
-        # was the line detected in the last iteration?
-        self.detected = False
+    def __init__(self, N=10):
         # x values of the last n fits of the line
         self.recent_xfitted = []
         # average x values of the fitted line over the last n iterations
@@ -9,7 +10,22 @@ class Line:
         # polynomial coefficients averaged over the last n iterations
         self.best_fit = None
         # polynomial coefficients for the most recent fit
-        self.current_fit = []
-        # radius of curvature of the line in some units
-        self.radius_of_curvature = None
-        # distance in meters of vehicle center from the line
+        self.N = N
+
+        self.fits = np.array([[]]).reshape((0, 3))
+
+        self.__i = 0
+
+    def running_mean(self, var):
+        cumsum = np.cumsum(var, axis=0)
+        return ((cumsum[self.N:] - cumsum[:-self.N]) / float(self.N))[-1]
+
+    def update_fit(self, fit):
+        self.fits = np.concatenate((self.fits, [fit]))
+
+        if self.__i >= self.N:
+            self.best_fit = self.running_mean(self.fits)
+        else:
+            self.best_fit = fit
+
+        self.__i += 1
